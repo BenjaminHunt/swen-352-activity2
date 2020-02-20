e6053f573e32833e90398a1f094ee72de0581eec
sjc5897 <sjc5897@g.rit.edu>
2020-02-20 17:00:34 -0500

    Completed Unit Tests for test_ext_api_interface.py

--- tests/test_ext_api_interface.py
+++ tests/test_ext_api_interface.py
@@ -50,14 +50,133 @@ class MyTestCase(unittest.TestCase):
             get_mock.return_value.json.return_value = data
             assert book.is_book_available("Das Kapital") == False
 
-    def test_is_book_available_the_book(self):
+    def test_is_book_available_found_book(self):
         data = {"start":0,"num_Found":1,"numFound":1,"docs":[{"title_suggest":"Das Kapital"}]}
         book = self.get_api()
         with patch.object(requests,'get') as get_mock:
             get_mock.return_value.status_code = 200
             get_mock.return_value.json.return_value = data
             assert book.is_book_available("Das Kapital") == True
+    #test_is_book_available end
 
+    #test_books_by_author start
+    def test_books_by_author_non_200(self):
+        book = self.get_api()
+        with patch.object(requests, 'get') as get_mock:
+            get_mock.return_value.status_code = 201
+            assert book.books_by_author("Karl Marx") == []
 
+    def test_books_by_author_no_books(self):
+        data = {
+            'start':0,
+            'num_Found':0,
+            'numFound':0,
+            'docs':[]
+        }
+        book = self.get_api()
+        with patch.object(requests,'get') as get_mock:
+            get_mock.return_value.status_code = 200
+            get_mock.return_value.json.return_value = data
+            assert book.books_by_author("Karl Marx") == []
+
+    def test_books_by_author_found_book(self):
+        data = {"start":0,"num_Found":1,"numFound":1,"docs":[{"title_suggest":"Das Kapital"}]}
+        book = self.get_api()
+        with patch.object(requests,'get') as get_mock:
+            get_mock.return_value.status_code = 200
+            get_mock.return_value.json.return_value = data
+            assert book.books_by_author("Karl Marx") == ["Das Kapital"]
+    #test_books_by_author end
+
+    #test_get_book_info start
+    def test_get_book_info_non_200(self):
+        book = self.get_api()
+        with patch.object(requests, 'get') as get_mock:
+            get_mock.return_value.status_code = 201
+            assert book.get_book_info("Das Kapital") == []
+
+    def test_get_book_info_no_book(self):
+        data = {
+            'start':0,
+            'num_Found':0,
+            'numFound':0,
+            'docs':[]
+        }
+        book = self.get_api()
+        with patch.object(requests,'get') as get_mock:
+            get_mock.return_value.status_code = 200
+            get_mock.return_value.json.return_value = data
+            assert book.get_book_info("Das Kapital") == []
+
+    def test_get_book_info_found_book(self):
+        data = {"start":0,"num_Found":1,"numFound":1,
+                "docs":[
+                    {'title_suggest':"Das Kapital",
+                     'title':"Das Kapital",
+                     'publisher':"Verlag von Otto Meisner",
+                     'publish_year':"1867",
+                     'language':"German"}
+                ]}
+        book = self.get_api()
+        with patch.object(requests,'get') as get_mock:
+            get_mock.return_value.status_code = 200
+            get_mock.return_value.json.return_value = data
+            assert book.get_book_info("Das Kapital") == [
+                {'title':"Das Kapital",
+                'publisher':"Verlag von Otto Meisner",
+                 'publish_year':"1867",
+                 'language':"German"}
+            ]
+    #test_get_book_info end
+
+    #test_get_ebooks start
+    def test_get_ebook_non_200(self):
+        book = self.get_api()
+        with patch.object(requests, 'get') as get_mock:
+            get_mock.return_value.status_code = 201
+            assert book.get_ebooks("Das Kapital") == []
+
+    def test_get_ebook_no_book(self):
+        data = {
+            'start': 0,
+            'num_Found': 0,
+            'numFound': 0,
+            'docs': []
+        }
+        book = self.get_api()
+        with patch.object(requests, 'get') as get_mock:
+            get_mock.return_value.status_code = 200
+            get_mock.return_value.json.return_value = data
+            assert book.get_ebooks("Das Kapital") == []
+
+    def test_get_ebook_found_book(self):
+        data = {"start": 0, "num_Found": 1, "numFound": 1,
+                "docs": [
+                    {'title_suggest': "Das Kapital",
+                     'title': "Das Kapital",
+                    'ebook_count_i':10}
+                ]}
+        book = self.get_api()
+        with patch.object(requests, 'get') as get_mock:
+            get_mock.return_value.status_code = 200
+            get_mock.return_value.json.return_value = data
+            assert book.get_ebooks("Das Kapital") == [
+                {'title': "Das Kapital",
+                 'ebook_count': 10,
+                }
+            ]
+    def test_get_ebook_found_book_but_0_ebooks(self):
+        data = {"start": 0, "num_Found": 1, "numFound": 1,
+                "docs": [
+                    {'title_suggest': "Das Kapital",
+                     'title': "Das Kapital",
+                    'ebook_count_i':0}
+                ]}
+        book = self.get_api()
+        with patch.object(requests, 'get') as get_mock:
+            get_mock.return_value.status_code = 200
+            get_mock.return_value.json.return_value = data
+            assert book.get_ebooks("Das Kapital") == [
+            ]
 if __name__ == '__main__':
     unittest.main()
